c1711a858feec63d145432c6e978f62a9c426d00
Рита Матвеева <rmatveeva1@gmail.com>
2022-03-21 22:22:36 +0300

    .

--- WpfTest/MainWindow.xaml.cs
+++ WpfTest/MainWindow.xaml.cs
@@ -30,62 +30,75 @@ namespace WpfTest
         public static int pv;
         String[,] Info;
         DateTime now;
+        StackPanel[] panels;
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer();
+            timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             now = DateTime.Now;
             timer.Start();
 
         }
+        private void Result()
+        {
+            timer.Stop();
+            MessageBox.Show("Тест окончен.\nНачало: "+now.ToString()+"\nКонец: "+DateTime.Now.ToString()+"\nПравильных ответов: " + k);
+            btnCheck.IsEnabled = false;
+        }
         void timer_Tick(object sender, EventArgs e)
         {
             lblTime.Content = DateTime.Now.Subtract(now).ToString(@"mm\:ss");
             if (lblTime.Content.ToString() == "10:00")
-                MessageBox.Show("Тест окончен.\nПравильных ответов: " + k); ;
+                Result();
         }
 
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
+            
             for (int j=0;j<ColVoprt-1;j++)
             {
                 int var = Convert.ToInt32(Info[j,6]);
                 if (rb[j,var-1].IsChecked==true)
                 {
                     k++;
-                    rb[j, var - 1].Background = new SolidColorBrush(Colors.Green);
+                    panels[j].Background = new SolidColorBrush(Colors.LightGreen);
+                    
                 }
                 else
-                    rb[j, var - 1].Background = new SolidColorBrush(Colors.Red);
+                {
+                    panels[j].Background = new SolidColorBrush(Colors.OrangeRed);
+                }
             }
-            MessageBox.Show("Тест окончен.\nПравильных ответов: " + k);
+            Result();
             k = 0;
         }
         private void Window_Initialized(object sender, EventArgs e)
         {
             ReadExcelMethod();
             rb = new RadioButton[ColVoprt - 1, 5];
+            panels = new StackPanel[ColVoprt - 1];
             for (int j = 0; j < ColVoprt-1; j++)
             {
-                StackPanel dynamicStackPanel = new StackPanel();
-                dynamicStackPanel.Background = new SolidColorBrush(Colors.LightBlue);
-                dynamicStackPanel.Orientation = Orientation.Vertical;
+                panels[j] = new StackPanel();
+                panels[j].Background = new SolidColorBrush(Colors.LightBlue);
+                panels[j].Orientation = Orientation.Vertical;
 
                 lq = new Label();
                 lq.Content = Info[j,0];
                 lq.FontSize = 23;
-                dynamicStackPanel.Children.Add(lq);
+                panels[j].Children.Add(lq);
                 for (int i = 0; i < 5; i++)
                 {
                     rb[j,i] = new RadioButton();
                     rb[j,i].FontSize = 15;
                     rb[j,i].Content = Info[j, i + 1];
-                    dynamicStackPanel.Children.Add(rb[j,i]);
+                    panels[j].Children.Add(rb[j,i]);
                 }
-                dynamicStackPanel.Margin = new Thickness(3, 5, 0, 0);
-                StackMain.Children.Add(dynamicStackPanel);
+                panels[j].Margin = new Thickness(3, 5, 0, 0);
+                StackMain.Children.Add(panels[j]);
             }
             
             k = 0;
